7bb4aa369532ed0c41c0e2a8f2e800ee764ba8f5
Phuong <phambuinamphuong@gmail.com>
2024-04-07 03:22:16 +0700

    restore temas template

--- routes/template.js
+++ routes/template.js
@@ -3,41 +3,45 @@ const path = require('path');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    
+
     const pick = [
         {
-            image : 'image/t1.png',
+            image: 'image/t1.png',
             title: 'Automation Solution',
             type: 'Business',
         },
         {
-            image : 'image/t2.png',
+            image: 'image/t2.png',
             title: 'Electronic components',
             type: 'Product & Business',
         },
         {
-            image : 'image/t3.png',
+            image: 'image/t3.png',
             title: 'Camera and Robot',
             type: 'Solution & Blog',
         },
         {
-            image : 'image/t4.png',
+            image: 'image/t4.png',
             title: 'Startups Solution',
             type: 'Business',
         },
         {
-            image : 'image/t5.png',
+            image: 'image/t5.png',
             title: 'Dev Book',
             type: 'Review',
         },
         {
-            image : 'image/t6.png',
+            image: 'image/t6.png',
             title: 'Dev Conf',
             type: 'Conferences & Events',
         },
     ]
 
     res.render('home/pages/template', { title: 'Template', pick: pick });
+});
+
+router.get("/temas", (req, res) => {
+    res.render('template/temas/home/pages/home', { title: 'Temas' });
 });
 
 module.exports = router;
