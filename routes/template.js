379b64ccaf4f094601ed0e5680f0a4f42c5e52fc
Phi Long <nguyenphilong.it.123@gmail.com>
2024-04-17 10:59:09 +0700

    update lips

--- routes/template.js
+++ routes/template.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { title } = require('process');
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -77,16 +78,131 @@ router.get("/thegioiic", (req, res) => {
     const newProducts = [
         {
             image: '/template/thegioiic/img/product.webp',
-            title: "Vinasemi 8858-I Máy Khò Cầm Tay 700W, 220VAC, 100-480ºC",
+            title: "Vinasemi 8858-I Hand Held Hot Air Gun 700W, 220VAC, 100-480ºC",
             price: '$39.474',
+            unit: 'PC',
             stock: '38'
         }
     ]
-    res.render('template/thegioiic/home/pages/home', { title: 'Thegioiic', newProducts: newProducts });
+    const typeProducts = [
+        {
+            name: 'ICs',
+            details: ['Processors MPU & Microcontrollers MCU', 'Programmable Logic Circuits',
+            'Memory ICs', 'Power Management ICs', 'Interface ICs'],
+            products: [
+                {
+                    image: '/template/thegioiic/img/ics_product.webp',
+                    title: "ATMEGA168PA-AU, 8bit AVR Microcontroller, 20MHz, 16kB Flash, 32 Pin TQFP",
+                    price: '$39.474',
+                    unit: 'PC',
+                    stock: '38'
+                }
+            ]
+        },
+        {
+            name: 'Boards, Modules, Debuggers',
+            details: ['GPS, GSM, GPRS Modules', 'Programmer, Debugger',
+            'RF, WiFi, Bluetooth Modules', 'Power Management ICs'],
+            products: [
+                {
+                    image: '/template/thegioiic/img/boards_product.webp',
+                    title: "STM32F103C8T6 Board",
+                    price: '$39.474',
+                    unit: 'PC',
+                    stock: '38'
+                }
+            ]
+        },
+        {
+            name: 'Connectors, Sockets',
+            details: ['Headers & Wire Housings', 'IC Sockets', 'Terminal Blocks'],
+            products: [
+                {
+                    image: '/template/thegioiic/img/connectors_product.webp',
+                    title: "9 Pin D-sub Connector Male 2 Row Solder Wire",
+                    price: '$39.474',
+                    unit: 'PC',
+                    stock: '38'
+                }
+            ]
+        }
+    ]
+    res.render('template/thegioiic/home/pages/home', { title: 'Thegioiic', newProducts: newProducts, typeProducts: typeProducts });
 });
 
 router.get("/lips", (req, res) => {
-    res.render('template/lips/home/pages/home', { title: 'LIPS' });
+    const typeProducts = [
+        {
+            name: 'Active Stereo Camera',
+            products: [
+                {
+                    title: 'LIPSedge™ AE400',
+                    image: '/template/lips/img/product1.png',
+                    description: 'Ruggedized Industrial 3D Depth Camera',
+                    FOV: '87° x 58° x 95°',
+                    shutter: 'Global Shutter / IMU',
+                    range: '0.52 ~ 10+m'
+                },
+                {
+                    title: 'LIPSedge™ AE430',
+                    image: '/template/lips/img/product2.png',
+                    description: 'Ruggedized Industrial 3D Depth Camera',
+                    FOV: '87° x 58° x 95°',
+                    shutter: 'Global Shutter / IMU',
+                    range: '0.52 ~ 10+m'
+                },
+                {
+                    title: 'LIPSedge™ AE450',
+                    image: '/template/lips/img/product3.png',
+                    description: 'Ruggedized Industrial 3D Depth Camera',
+                    FOV: '87° x 58° x 95°',
+                    shutter: 'Global Shutter / IMU',
+                    range: '0.52 ~ 10+m'
+                }
+            ]
+        },
+        {
+            name: 'Time-of-Flight Camera',
+            products: [
+                {
+
+                }
+            ]
+        },
+        {
+            name: 'Structured-Light Camera',
+            products: [
+                {
+                    
+                }
+            ]
+        },
+        {
+            name: 'Edge Accelerator',
+            products: [
+                {
+                    
+                }
+            ]
+        },
+        {
+            name: 'Multiple Dimensioning',
+            products: [
+                {
+                    
+                }
+            ]
+        },
+        {
+            name: 'Facial Recognition',
+            product: [
+                {
+                    
+                }
+            ]
+        }
+    ]
+    res.render('template/lips/home/pages/home', { title: 'LIPS', typeProducts: typeProducts });
 });
 
 module.exports = router;
